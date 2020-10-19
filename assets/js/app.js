fb818596675b27428caf9a7b89f60c7039dc6eb0
Saif <saifeddinerashed@icloud.com>
2020-10-19 23:22:56 +0200

    berichten sectie & notificatie pagina

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,41 @@
-$(function() {
-    $("#klikmij-button").on("click", function() {
-       $("#klikmij-toggle").toggle();
-    });
 
-    $("#klikmij-toggle img").on("click", function() {
-        alert($(this).attr("alt"));
-    })
+
+
+$(document).ready(function(){
+
+    $('.items').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
 });
