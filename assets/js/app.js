9aa7715219aa9efbaae52a73b18ba887d2500fcd
Saif <saifeddinerashed@icloud.com>
2020-10-23 15:05:34 +0200

    Site aanpassing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,36 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
 
-
-
-$(document).ready(function(){
 
     $('.items').slick({
-        dots: true,
-        infinite: true,
-        speed: 800,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        slidesToShow: 4,
+        dots:           true,
+        infinite:       true,
+        speed:          800,
+        autoplay:       true,
+        autoplaySpeed:  2000,
+        slidesToShow:   4,
         slidesToScroll: 4,
-        responsive: [
+        responsive:     [
             {
                 breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
+                settings:   {
+                    slidesToShow:   3,
                     slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
+                    infinite:       true,
+                    dots:           true
                 }
             },
             {
                 breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
+                settings:   {
+                    slidesToShow:   2,
                     slidesToScroll: 2
                 }
             },
             {
                 breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
+                settings:   {
+                    slidesToShow:   1,
                     slidesToScroll: 1
                 }
             }
@@ -39,3 +38,5 @@ $(document).ready(function(){
         ]
     });
 });
+
+
