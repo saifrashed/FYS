b4ae6254c929da5e6336da5b09ebd2ad2c861f3a
Saif <saifeddinerashed@icloud.com>
2020-11-27 13:28:25 +0100

    Sorry

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,19 +3,19 @@ $(document).ready(function () {
     /*
      * Declaration of variables
      */
-    var database = null;
-    var user = null;
-    var translation = null;
-    var notification = null;
-    var genders = null;
-    var userData = null;
-    var hobbiesList = null;
-    var vacationList = null;
-    var hobbiesUserList = null;
+    var database         = null;
+    var user             = null;
+    var translation      = null;
+    var notification     = null;
+    var genders          = null;
+    var userData         = null;
+    var hobbiesList      = null;
+    var vacationList     = null;
+    var hobbiesUserList  = null;
     var vacationUserList = null;
-    var deleteHobbies = null;
-    var deleteVacation = null;
-    var matchData = null;
+    var deleteHobbies    = null;
+    var deleteVacation   = null;
+    var matchData        = null;
 
     /**
      * Init
@@ -25,20 +25,25 @@ $(document).ready(function () {
      */
 
     async function init() {
+
+        // parameters
+        const urlParams   = new URLSearchParams(window.location.search);
+        const searchQuery = urlParams.get('query') || "";
+
         // variables
-        database = new Database("https://api.fys.cloud/", "fys_is106_5.Pk9ggWAU7qg9EXTv", "fys_is106_5_dev", "dev");
-        user = new User();
-        translation = new Translation();
-        notification = new Notifications();
-        genders = await database.getGenders();
-        userData = await user.getUserData(user.userID);
-        hobbiesList = await database.getInterestList("hobbies");
-        vacationList = await database.getInterestList("vacations");
-        hobbiesUserList = await user.getInterest("hobbies", user.userID);
+        database         = new Database("https://api.fys.cloud/", "fys_is106_5.Pk9ggWAU7qg9EXTv", "fys_is106_5_dev", "dev");
+        user             = new User();
+        translation      = new Translation();
+        notification     = new Notifications();
+        genders          = await database.getGenders();
+        userData         = await user.getUserData(user.userID);
+        hobbiesList      = await database.getInterestList("hobbies");
+        vacationList     = await database.getInterestList("vacations");
+        hobbiesUserList  = await user.getInterest("hobbies", user.userID);
         vacationUserList = await user.getInterest("vacations", user.userID);
-        deleteHobbies = await user.deleteInterest("hobbies", hobbiesUserList, user.userID);
-        deleteVacation = await user.deleteInterest("vacation", vacationUserList, user.userID);
-        matchData = await database.getMatches(user.userID, "%%")
+        deleteHobbies    = await user.deleteInterest("hobbies", hobbiesUserList, user.userID);
+        deleteVacation   = await user.deleteInterest("vacation", vacationUserList, user.userID);
+        matchData        = await database.getMatches(user.userID, "%" + searchQuery + "%");
 
         // functions
         user.authenticateUser(user.userID);
@@ -46,13 +51,14 @@ $(document).ready(function () {
         displayUserData(userData);
         populateInterests(hobbiesList, vacationList);
         populateUserInterests(hobbiesUserList, vacationUserList);
-        //deleteInterests(deleteHobbies, deleteVacation);
-
-
-        populateMatches(matchData)
-
+        populateUserEditInterests(hobbiesUserList, vacationUserList);
 
         //matchings
+        populateMatches(matchData);
+
+
+        console.log(hobbiesUserList)
+
 
     }
 
@@ -66,13 +72,13 @@ $(document).ready(function () {
         for (let i = 0; i < matchData.length; i++) {
             $("#matchRow").append("<div class=\"col-lg-4 col-md-6 mb-4\">\n" +
                 "                    <div class=\"card h-100\">\n" +
-                "                        <a href=\"profileDetail.html\">\n" +
+                "                        <a href=\"profileDetail.html?userID=" + matchData[i].userID + "\">\n" +
                 "                            <div class=\"profile-img\"\n" +
                 "                                 style=\"background-image: url('assets/img/stock/stock-7.jpg')\"></div>\n" +
                 "                        </a>\n" +
                 "                        <div class=\"card-body\">\n" +
                 "                            <h4 class=\"card-title\">\n" +
-                "                                <a href=\"profileDetail.html\">" + matchData[i].firstName + " " + matchData[i].lastName + "</a>\n" +
+                "                                <a href=\"profileDetail.html?userID=" + matchData[i].userID + "\">" + matchData[i].firstName + " " + matchData[i].lastName + "</a>\n" +
                 "                            </h4>\n" +
                 "                            <p class=\"card-text\">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam\n" +
                 "                                aspernatur!</p>\n" +
@@ -142,6 +148,47 @@ $(document).ready(function () {
      * @param vacations
      */
     function populateUserInterests(hobbies, vacations) {
+
+
+        hobbies.map(function (value, key) {
+            $("#userProfile-hobbies").append("<li class=\"list-group-item\">" + value.description + "</li>")
+        });
+
+
+        vacations.map(function (value, key) {
+            $("#userProfile-vacations").append("<div class=\"card\">\n" +
+                "                                        <div class=\"card-header\" id=\"headingOne\">\n" +
+                "                                            <h2 class=\"mb-0\">\n" +
+                "                                                <button aria-controls=\"collapseOne\" aria-expanded=\"true\"\n" +
+                "                                                        class=\"btn btn-link btn-block text-left\"\n" +
+                "                                                        data-target=\"#collapseOne\"\n" +
+                "                                                        data-toggle=\"collapse\" type=\"button\">\n" +
+                "                                                    " + value.destination + "\n" +
+                "                                                </button>\n" +
+                "                                            </h2>\n" +
+                "                                        </div>\n" +
+                "\n" +
+                "                                        <div aria-labelledby=\"headingOne\" class=\"collapse\"\n" +
+                "                                             data-parent=\"#userProfile-vacations\"\n" +
+                "                                             id=\"collapseOne\">\n" +
+                "                                            <div class=\"card-body\">\n" +
+                "                                                " + value.description + "\n" +
+                "                                                <a class=\"btn btn-outline-primary m-1\"\n" +
+                "                                                   href=\" " + value.url + " \"\n" +
+                "                                                   target=\"_blank\">Bekijk vakantie <i\n" +
+                "                                                        class=\"fas fa-plane\"></i></a>\n" +
+                "                                            </div>\n" +
+                "                                        </div>\n" +
+                "                                    </div>")
+        });
+    }
+
+    /**
+     * Populates user page hobbies and vacations
+     * @param hobbies
+     * @param vacations
+     */
+    function populateUserEditInterests(hobbies, vacations) {
         console.log(hobbies);
         console.log(vacations);
     }
@@ -241,7 +288,7 @@ $(document).ready(function () {
             var selectedVacation = vacationList.filter(obj => {
                 return obj.vacationID == inputSelectedVacation.val();
             });
-            var addVacation = await user.addInterest("vacations", selectedVacation[0].vacationID, user.userID)
+            var addVacation      = await user.addInterest("vacations", selectedVacation[0].vacationID, user.userID)
             if (addVacation) {
                 notification.success("Vakantie toegevoegd!");
             }
@@ -265,6 +312,17 @@ $(document).ready(function () {
             console.log(e);
         }
     });
+
+    // deletes vacation
+    $("#userEdit-deleteVacation").click(function (e) {
+        try {
+            e.preventDefault();
+            console.log("klik werkt");
+        } catch (e) {
+            console.log(e);
+        }
+    });
+
 
     //Adds hobbies
     $("#userEdit-addHobby").click(async function () {
@@ -290,17 +348,20 @@ $(document).ready(function () {
 
     });
 
-//     deletes interests and vacations
-//     $("#userEdit-deleteHobby").click(function (e) {
-//         e.preventDefault();
-//         console.log("klik werkt");
-// try{
-//     var
-//
-// }catch (e) {
-//     console.log(e);
-// }
-//   });
+    // deletes hobby
+    $("#userEdit-deleteHobby").click(function (e) {
+        try {
+            e.preventDefault();
+            console.log("klik werkt");
+        } catch (e) {
+            console.log(e);
+        }
+    });
+
+    // search button
+    $("#searchButton").click(function (e) {
+        location.href = "./profileOverview.html?query=" + $("#searchQuery").val();
+    });
 
     /**
      * END: User section
@@ -318,7 +379,7 @@ $(document).ready(function () {
 
     // Displays the users friendlist
     $('.show-friends-button').on('click', function () {
-        var friendList = $('.friendlist');
+        var friendList        = $('.friendlist');
         var friendListOverlay = $('.page-overlay');
 
         friendList.toggleClass("show");
@@ -327,7 +388,7 @@ $(document).ready(function () {
 
     // Friendlist page overlay is clickable
     $('.page-overlay').on('click', function () {
-        var friendList = $('.friendlist');
+        var friendList        = $('.friendlist');
         var friendListOverlay = $('.page-overlay');
 
         friendList.toggleClass("show");
